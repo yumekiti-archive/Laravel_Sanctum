53921e187d77b8f004923a908ba382d031d238e0
yumekiti <yumekiti120440@gmail.com>
2021-11-24 14:16:22 +0900

    delete: vue

--- app/src/libs/client.ts
+++ app/src/libs/client.ts
@@ -2,12 +2,6 @@ import Axios from 'axios'
 
 const client = Axios.create({
   baseURL: 'http://localhost:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true'
-  },
-  withCredentials: true,
 })
 
 export default client;
